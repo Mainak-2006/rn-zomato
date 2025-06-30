eb739cb232b28dc358d9143867b917a506d48db2
MAINAK KUNDU <mainakkundu13@gmail.com>
2025-06-30 21:47:05 +0530

    Update FireBaseConfig.js
    remove the original configuration

--- config/FireBaseConfig.js
+++ config/FireBaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBMNZUfoumM3O_YdZbuaCxeosKqgIRY0tg",
-  authDomain: "zomato-7b56d.firebaseapp.com",
-  projectId: "zomato-7b56d",
-  storageBucket: "zomato-7b56d.firebasestorage.app",
-  messagingSenderId: "384297145885",
-  appId: "1:384297145885:web:fb007d56ca7ae7722598f4",
-  measurementId: "G-YPGY26H7EH"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 
